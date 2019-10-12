0dc92aab9c33a806e15e8ef11df911e8cebfe974
Vinayak Redekar <redekar62.vinayak@gmail.com>
2019-10-12 16:09:55 +0800

    initaial commit

---
+++ src/main/java/com/openshifttutor/basics/springbootopenshift/SpringbootOpenshiftApplication.java
@@ -0,0 +1,21 @@
+package com.openshifttutor.basics.springbootopenshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringbootOpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOpenshiftApplication.class, args);
+	}
+	
+	@GetMapping(name = "hello")
+	public String welcomeOpenshit() {
+		return "Welcome to OpenShift!";
+	}
+
+}
